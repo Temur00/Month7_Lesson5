6b4bcf1e44aa32de1ca45a1c0bfd7596e4e6b63b
Temur <shernazarovtemurcom001@gmail.com>
2024-02-03 02:15:31 +0500

    project set up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Browser as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages";
+
+const App = () => {
+  return (
+    <div>
+      <Router></Router>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import { Home } from "./Home";
+import { Catalog } from "./Catalog";
+import { Filter } from "./Filter";
+
+export { Home, Catalog, Filter };
